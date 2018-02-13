f9033f396f802387a6a63b285ad503d9c22a4ad2
Nathan <nathan.hsu@icloud.com>
2018-02-13 13:33:04 -0800

    boilerplate

---
+++ Box.h
@@ -0,0 +1,13 @@
+//
+//  Box.h
+//  Box
+//
+//  Created by Nathan Hsu on 2018-02-13.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+@end
---
+++ Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  Box
+//
+//  Created by Nathan Hsu on 2018-02-13.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
